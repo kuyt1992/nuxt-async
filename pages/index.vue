4528a4f677d6bd9a33f16f1f395e4a2f5c2dc222
kuyt1992 <kuyt1992@gmail.com>
2021-09-07 18:15:30 +0900

    feat: エラー処理追加

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div>
       <ul>
         <li v-for="user in users" :key="user.id">
-          {{ user.id }}. {{ user.name }}
+          {{ user.id }}. {{ user.name }}, {{ user.company.name }}
         </li>
       </ul>
     </div>
@@ -12,14 +12,17 @@
 
 <script>
 const axios = require('axios')
-let url = 'https://jsonplaceholder.typicode.com/users'
+let url = 'https://jsonplaceholder.typicode.com/usersss'
 
 export default {
-  asyncData({params}) {
+  asyncData({ params, error }) {
     return axios.get(url)
     .then((res) => {
       return { users: res.data }
     })
+    .catch((e => {
+      error({ statusCode: e.response.status, message: e.message})
+    }))
   }
 }
 </script>
