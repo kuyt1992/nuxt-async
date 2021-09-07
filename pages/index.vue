b3da7e3afd8d0c87baaac5607d2bc2e59565b0e5
kuyt1992 <kuyt1992@gmail.com>
2021-09-07 18:09:23 +0900

    feat: jsonplaceholder apiからuserデータを取得し表示する

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,25 @@
 <template>
-  <Tutorial/>
+  <section class="container">
+    <div>
+      <ul>
+        <li v-for="user in users" :key="user.id">
+          {{ user.id }}. {{ user.name }}
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {}
+const axios = require('axios')
+let url = 'https://jsonplaceholder.typicode.com/users'
+
+export default {
+  asyncData({params}) {
+    return axios.get(url)
+    .then((res) => {
+      return { users: res.data }
+    })
+  }
+}
 </script>
